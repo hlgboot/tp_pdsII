d8b3cc0db0e5fcae9455682a6f2fd006b70db24e
GABRIEL SETTE CÂMARA PIANCASTELLI <2022079803>
2025-10-07 09:43:29 -0300

    Arquivo de teste adicionado - Gabriel

---
+++ Validacao_acesso.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // Arquivo de teste para validar o acesso
+    std::cout << "Configuracao de ambiente concluida com sucesso!" << std::endl;
+    return 0;
+}
